1b70be3e78ff741c175d8d06d54101b1c219a5d5
lukeduke2222 <lalbin20@loyolaprep.org>
2021-12-09 01:59:23 -0700

    uploaded board

--- back-end/server.js
+++ back-end/server.js
@@ -113,4 +113,4 @@ app.put('/api/notes/:id', async (req, res) => {
 
 
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3001, () => console.log('Server listening on port 3001!'));
